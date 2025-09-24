a62df274c16424573cbe9cc7ab10f90665e81599
rarmand <holik.alek@gmail.com>
2025-09-24 13:31:55 +0200

    changed favicon

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div>
+            Award-winning custom designs and digital branding solutions
+
+            With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, 
+            and engaging brand experiences. Find out more about our services.
+            
+        </div>
+    );
+}
