e129dc0b83abb8f7badef23c354ded6bae8c59b5
rarmand <holik.alek@gmail.com>
2025-09-26 14:10:07 +0200

    added content to HOME

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,7 +3,17 @@ import styles from '../page.module.css';
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>HOME</main>
+      <main className={styles.main}>
+        
+        HOME
+        <div className='description'>
+          <h1>Award-winning custom designs and digital branding solutions</h1>
+          <p>With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, 
+  and engaging brand experiences. Find out more about our services.</p>
+  
+          <button>Learn more</button>
+        </div>
+        </main>
     </div>
   );
 }
