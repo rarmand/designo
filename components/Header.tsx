9e8f56773fc15398f58594ee8ef3b9a7b7cd21f9
rarmand <holik.alek@gmail.com>
2025-09-29 14:44:22 +0200

    created Header for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,7 @@
-import Link from 'next/link';
+// import Link from 'next/link';
+// 'use client';
+
+import Image from 'next/image';
 
 export default function Header() {
   const locationsLink = 'Locations';
@@ -6,19 +9,30 @@ export default function Header() {
   const aboutLink = 'Our company';
   const homeLink = 'Home';
 
+  // TODO: rózne logo w zalezności od view mode
+  // TODO: jak ustawić ze hamburger pojawia się tylko gdy jest mobile
   return (
-    <div>
-      <nav className='container relative flex flex-wrap items-center justify-between mx-auto'>
-        <Link href='/' className='font-bold text-3xl'>
-          {homeLink}
-        </Link>
+    <header>
+      <Image src='/logo-dark.png' alt='designo logo' width={200} height={27} />
+      <Image
+        src='/mobile/icon-hamburger.svg'
+        alt='menu'
+        width={20}
+        height={20}
+      />
+    </header>
+    // <div>
+    //   <nav className='container relative flex flex-wrap items-center justify-between mx-auto'>
+    //     <Link href='/' className='font-bold text-3xl'>
+    //       {homeLink}
+    //     </Link>
 
-        <div className='space-x-4 text-xl'>
-          <Link href='/about'>{aboutLink}</Link>
-          <Link href='/locations'>{locationsLink}</Link>
-          <Link href='/contact'>{contactLink}</Link>
-        </div>
-      </nav>
-    </div>
+    //     <div className='space-x-4 text-xl'>
+    //       <Link href='/about'>{aboutLink}</Link>
+    //       <Link href='/locations'>{locationsLink}</Link>
+    //       <Link href='/contact'>{contactLink}</Link>
+    //     </div>
+    //   </nav>
+    // </div>
   );
 }
