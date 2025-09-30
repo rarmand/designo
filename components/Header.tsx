77bb190b563bcb0fb17f42b49d474615dfcfcfd9
rarmand <holik.alek@gmail.com>
2025-09-30 15:22:49 +0200

    testing menu and nav in header, creating buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,41 +1,73 @@
 // import Link from 'next/link';
-// 'use client';
+'use client';
 
+import Link from 'next/link';
 import styles from './Header.module.scss';
 import Image from 'next/image';
+import text from '../i18n/en.json';
+import { useState } from 'react';
 
 export default function Header() {
-  const locationsLink = 'Locations';
-  const contactLink = 'Contact';
-  const aboutLink = 'Our company';
-  const homeLink = 'Home';
-
   // TODO: rózne logo w zalezności od view mode
   // TODO: jak ustawić ze hamburger pojawia się tylko gdy jest mobile
   // TODO: nadać RWD
   // BEM - block element modifier
+  const [open, setOpen] = useState(false);
+
+  const onLogoClick = () => {};
+
+  const onMenuButtonClick = () => {
+    // this.open = !open;
+  };
+
   return (
     <header className={styles.header}>
-      <Image src='/logo-dark.png' alt='designo logo' width={200} height={27} />
-      <Image
-        src='/mobile/icon-hamburger.svg'
-        alt='menu'
-        width={20}
-        height={20}
-      />
-    </header>
-    // <div>
-    //   <nav className='container relative flex flex-wrap items-center justify-between mx-auto'>
-    //     <Link href='/' className='font-bold text-3xl'>
-    //       {homeLink}
-    //     </Link>
+      <div className={styles.header__main}>
+        <button onClick={onLogoClick} type='button' aria-label='Click the icon'>
+          <Image
+            src='/logo-dark.png'
+            alt={text['header.logo.alt']}
+            width={200}
+            height={27}
+          />
+        </button>
+        <button
+          onClick={onMenuButtonClick}
+          type='button'
+          // aria-label='Click the menu icon'
+          //aria-controls='primary-navigation'
+        >
+          <Image
+            src='/mobile/icon-hamburger.svg'
+            alt={text['header.hamburger.alt']}
+            width={20}
+            height={20}
+          />
+        </button>
+      </div>
 
-    //     <div className='space-x-4 text-xl'>
-    //       <Link href='/about'>{aboutLink}</Link>
-    //       <Link href='/locations'>{locationsLink}</Link>
-    //       <Link href='/contact'>{contactLink}</Link>
-    //     </div>
-    //   </nav>
-    // </div>
+      
+      <nav className={styles.header__menu}>
+        <ul id='primary-navigation' className='primary-navigation'>
+          <li>
+            <Link href='/about' aria-label='About our company'>
+              {text['header.link.about']}
+            </Link>
+          </li>
+
+          <li>
+            <Link href='/locations' aria-label='Locations'>
+              {text['header.link.locations']}
+            </Link>
+          </li>
+
+          <li>
+            <Link href='/contact' aria-label='Contact'>
+              {text['header.link.contact']}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
