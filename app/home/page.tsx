382e91a58fcd2419b00975bc4e66297a5f10f712
rarmand <holik.alek@gmail.com>
2025-10-09 13:49:58 +0200

    added styles and bgimg for introduction

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,20 @@
 import styles from './home.module.scss';
-import text from '../../i18n/en.json';
+import text from '@/i18n/en.json';
+import Image from 'next/image';
 
 export default function Home() {
+  const phoneImgPath = '/home/image-hero-phone.png';
+
   return (
     <main className={styles.home}>
-      <section className={styles.home__introduction}>
-        <h1 className={styles.home__header}>{text['home.title']}</h1>
-        <p className={styles.home__paragraph}>{text['home.description']}</p>
-      </section>
+      <section className={styles.home__header}>
+        <div className={styles.home__introduction}>
+          <h1 className={styles.home__title}>{text['home.title']}</h1>
+          <p>{text['home.description']}</p>
+        </div>
 
-      <button className={styles.home__button}>{text['home.button']}</button>
+        <button className={styles.home__button}>{text['home.button']}</button>
+        <Image src={phoneImgPath} alt='' width={100} height={200} />        </section>
     </main>
   );
 }
