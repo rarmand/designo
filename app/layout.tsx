b6870789abbf9fcc63c99969b9bcb25453d6aa37
rarmand <holik.alek@gmail.com>
2025-10-22 14:16:22 +0200

    added translation config to layout Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
 import { Jost } from 'next/font/google';
 import '@/styles/globals.scss';
-import text from '@/i18n/en.json';
 import Header from '@/components/Header';
+import { NextIntlClientProvider } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 
 const jost = Jost({
   subsets: ['latin'],
@@ -11,11 +12,20 @@ const jost = Jost({
   variable: '--font-jost',
 });
 
-// TODO: config of i18n lib
-export const metadata: Metadata = {
-  title: text['website.title'],
-  description: text['website.description'],
-};
+interface TranslationProps {
+  params: { locale: string };
+}
+
+export async function generateMetadata({
+  params: { locale },
+}: TranslationProps): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: 'Metadata' });
+
+  return {
+    title: t('title'),
+    description: t('description'),
+  };
+}
 
 /*
   The RootLayout component in the under code is responsible for 
@@ -24,6 +34,10 @@ export const metadata: Metadata = {
   - applies global styles, metadata, and shared UI components 
   - children prop represents the main content of each page, 
     dynamically inserted based on the current route.
+*/
+/*   
+  Wraping components with NextIntlClientProvider makes 
+  request i18n configuration available to Client Components.
 */
 
 export default function RootLayout({
@@ -34,8 +48,10 @@ export default function RootLayout({
   return (
     <html>
       <body className={jost.variable}>
-        <Header />
-        {children}
+        <NextIntlClientProvider>
+          <Header />
+          {children}
+        </NextIntlClientProvider>
       </body>
     </html>
   );
