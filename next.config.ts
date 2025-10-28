9bd28cc241daa91bc2b6c4aa15164b4fc0554fe9
rarmand <holik.alek@gmail.com>
2025-10-28 13:44:05 +0100

    installed svgr/webpack

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
+
 const nextConfig: NextConfig = {
   /* config options here */
   sassOptions: {
@@ -7,6 +8,35 @@ const nextConfig: NextConfig = {
     prependData: `
       @use "styles/variables.scss" as *;
     `,
+  },
+  webpack(config) {
+    // Znajdź istniejącą regułę loadera dla plików SVG
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.('.svg')
+    );
+
+    if (fileLoaderRule) {
+      // Wyklucz SVG z obecnej reguły Next.js
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    config.module.rules.push(
+      // Obsługa importu SVG jako ścieżka (opcjonalnie przez ?url)
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Obsługa SVG jako komponent Reacta z SVGR
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] },
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    return config;
   },
 };
 
