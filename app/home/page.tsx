279248061c971f75408b4b298909b39d149f4c07
rarmand <holik.alek@gmail.com>
2025-11-20 15:06:24 +0100

    future plans

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -13,6 +13,14 @@ import BlockLink from '@/components/BlockLink';
  * dodać prettier format na Ctrl Save
  *
  * zacząć pisać testy dla components
+ * 
+  1.  Podział Home Page na mniejsze komponenty
+	2.	Dobre testy tych komponentów
+  3.  Podział Header i Footer na mniejsze komponenty
+  4.  Testowanie komponentów.
+	5.	Rozszerzenie responsywności na tablet i desktop
+	6.	Przejście do Tailwind CSS - optional
+ * 
  *
  */
 
