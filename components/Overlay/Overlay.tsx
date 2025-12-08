b4a6fe7a30e742693da8d2a60e3d793631783f94
rarmand <holik.alek@gmail.com>
2025-12-08 16:16:47 +0100

    fix in Overlay

--- components/Overlay/Overlay.tsx
+++ components/Overlay/Overlay.tsx
@@ -4,14 +4,15 @@ import { useTranslations } from 'next-intl';
 import ButtonLink from '../ButtonLink';
 import styles from './Overlay.module.scss';
 
+// TODO: check if additional div is needed
 const Overlay = () => {
   const translator = useTranslations('Overlay');
 
   return (
-    <div className={styles.overlay}>
-      <div className={styles.overlay__content}>
-        <div className={styles.overlay__text}>
-          <h1 className={styles.overlay__title}>{translator('title')}</h1>
+    <div className={styles.overlayContainer}>
+      <div className={styles.overlay}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>{translator('title')}</h1>
           <p>{translator('description')}</p>
         </div>
         <ButtonLink route={'contact'} name={translator('button')}></ButtonLink>
