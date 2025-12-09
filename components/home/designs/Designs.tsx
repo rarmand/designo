f55a02161f66fb6d76fae0ac891dc888bafa50db
rarmand <holik.alek@gmail.com>
2025-12-09 14:46:40 +0100

    created Designs server component

---
+++ components/home/designs/Designs.tsx
@@ -0,0 +1,17 @@
+import BlockLink from '@/components/ui/BlockLink';
+import styles from './Designs.module.scss';
+import { getTranslations } from 'next-intl/server';
+
+async function Designs() {
+  const translator = await getTranslations('HomePage.designs');
+
+  return (
+    <section className={styles.designs}>
+      <BlockLink name={translator('web')} route='web' />
+      <BlockLink name={translator('app')} route='app' />
+      <BlockLink name={translator('graphic')} route='graphic' />
+    </section>
+  );
+}
+
+export default Designs;
