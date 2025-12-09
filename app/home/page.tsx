d941c78851c850cb7fe0ca1640ea5a3b8208f879
rarmand <holik.alek@gmail.com>
2025-12-09 14:54:27 +0100

    updated Home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,13 @@
 import styles from './Home.module.scss';
 import { getTranslations } from 'next-intl/server';
 import Image from 'next/image';
-import ButtonLink from '@/components/ButtonLink';
-import BlockLink from '@/components/BlockLink';
+import ButtonLink from '@/components/ui/ButtonLink';
+import Designs from '@/components/home/designs';
+import Principles from '@/components/home/principles';
 
 /*
  * * * TODO:
  *
- * jak pisać style BEM dla rozbudowanych elementów?
  * sprawdzić jak zapisać rozmiary obrazków, czy w HTML czy w CSS, zeby nie byly problemowe
  * dodać aria-labels do wszystkich wymaganych elementów zgodnie z NOTES
  * zainstalować lib do testowania, poczytać o narzędziach do testowania
@@ -29,17 +29,17 @@ export default async function HomePage() {
   return (
     <main className={styles.home}>
       {/*here*/}
-      <section className={styles.home__header}>
-        <div className={styles.home__introduction}>
-          <div className={styles.home__text}>
-            <h1 className={styles.home__title}>{translator('title')}</h1>
+      <section className={styles.header}>
+        <div className={styles.introduction}>
+          <div className={styles.text}>
+            <h1 className={styles.title}>{translator('title')}</h1>
             <p>{translator('description')}</p>
           </div>
 
           <ButtonLink name={translator('button')} route='about' />
         </div>
 
-        <div className={styles.home__phoneImg}>
+        <div className={styles.phoneImg}>
           <Image
             src={'/home/image-hero-phone.png'}
             alt={translator('image')}
@@ -53,77 +53,10 @@ export default async function HomePage() {
       </section>
 
       {/*here*/}
-      <section className={styles.home__designs}>
-        <BlockLink name={translator('designs.web')} route='web' />
-        <BlockLink name={translator('designs.app')} route='app' />
-        <BlockLink name={translator('designs.graphic')} route='graphic' />
-      </section>
+      <Designs />
 
       {/*here*/}
-      <section className={styles.home__principles}>
-        {/*here*/}
-        <div className={styles.home__principle}>
-          <div className={styles.home__principleImage}>
-            <div className={styles.home__principleBackgroundImage} />
-
-            <Image
-              src={'/home/illustration-passionate.svg'}
-              alt={translator('principles.passionate.alt')}
-              fill
-              className={styles.principleForegroundImage}
-            />
-          </div>
-
-          <div className={styles.home__principleDescription}>
-            <h3 className={styles.home__principleName}>
-              {translator('principles.passionate.name')}
-            </h3>
-            <p>{translator('principles.passionate.description')}</p>
-          </div>
-        </div>
-
-        {/*here*/}
-        <div className={styles.home__principle}>
-          <div className={styles.home__principleImage}>
-            <div className={styles.home__principleBackgroundImage} />
-
-            <Image
-              src={'/home/illustration-resourceful.svg'}
-              alt={translator('principles.resourceful.alt')}
-              fill
-              className={styles.principleForegroundImage}
-            />
-          </div>
-
-          <div className={styles.home__principleDescription}>
-            <h3 className={styles.home__principleName}>
-              {translator('principles.resourceful.name')}
-            </h3>
-            <p>{translator('principles.resourceful.description')}</p>
-          </div>
-        </div>
-
-        {/*here*/}
-        <div className={styles.home__principle}>
-          <div className={styles.home__principleImage}>
-            <div className={styles.home__principleBackgroundImage} />
-
-            <Image
-              src={'/home/illustration-friendly.svg'}
-              alt={translator('principles.friendly.alt')}
-              fill
-              className={styles.principleForegroundImage}
-            />
-          </div>
-
-          <div className={styles.home__principleDescription}>
-            <h3 className={styles.home__principleName}>
-              {translator('principles.friendly.name')}
-            </h3>
-            <p>{translator('principles.friendly.description')}</p>
-          </div>
-        </div>
-      </section>
+      <Principles />
     </main>
   );
 }
