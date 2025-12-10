baa6990c58e35081ccad0e79003584699d3d8263
rarmand <holik.alek@gmail.com>
2025-12-10 13:55:25 +0100

    added Principle to Principles list

--- components/home/principles/Principles.tsx
+++ components/home/principles/Principles.tsx
@@ -1,76 +1,12 @@
 import styles from './Principles.module.scss';
-import { getTranslations } from 'next-intl/server';
-import Image from 'next/image';
+import Principle from './Principle/Principle';
 
-// TODO: exclude one principle? is it possible?
-
-async function Principles() {
-  const translator = await getTranslations('HomePage.Principles');
-
+function Principles() {
   return (
     <section className={styles.principles}>
-      {/*here*/}
-      <div className={styles.principle}>
-        <div className={styles.principleImage}>
-          <div className={styles.principleBackgroundImage} />
-
-          <Image
-            src={'/home/illustration-passionate.svg'}
-            alt={translator('passionate.alt')}
-            fill
-            className={styles.principleForegroundImage}
-          />
-        </div>
-
-        <div className={styles.principleDescription}>
-          <h3 className={styles.principleName}>
-            {translator('passionate.name')}
-          </h3>
-          <p>{translator('passionate.description')}</p>
-        </div>
-      </div>
-
-      {/*here*/}
-      <div className={styles.principle}>
-        <div className={styles.principleImage}>
-          <div className={styles.principleBackgroundImage} />
-
-          <Image
-            src={'/home/illustration-resourceful.svg'}
-            alt={translator('resourceful.alt')}
-            fill
-            className={styles.principleForegroundImage}
-          />
-        </div>
-
-        <div className={styles.principleDescription}>
-          <h3 className={styles.principleName}>
-            {translator('resourceful.name')}
-          </h3>
-          <p>{translator('resourceful.description')}</p>
-        </div>
-      </div>
-
-      {/*here*/}
-      <div className={styles.principle}>
-        <div className={styles.principleImage}>
-          <div className={styles.principleBackgroundImage} />
-
-          <Image
-            src={'/home/illustration-friendly.svg'}
-            alt={translator('friendly.alt')}
-            fill
-            className={styles.principleForegroundImage}
-          />
-        </div>
-
-        <div className={styles.principleDescription}>
-          <h3 className={styles.principleName}>
-            {translator('friendly.name')}
-          </h3>
-          <p>{translator('friendly.description')}</p>
-        </div>
-      </div>
+      <Principle type='passionate' />
+      <Principle type='resourceful' />
+      <Principle type='friendly' />
     </section>
   );
 }
